e6598ee97c63ad43fe250c0523bb540d81760af5
sebygalla <sebygalla@hotmail.com>
2022-10-19 09:47:27 -0300

    responsive menu

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,12 @@
 function abrir (){
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
-    const closeMenu = document.querySelector('.nav__close');
+    const closeMenu = document.querySelector('.nav__menu--close');
    
 
     openButton.addEventListener('click', () => {
          menu.classList.add('nav__link--show');
+         menu.classList.add('.nav__menu--close')
     
     });
 
